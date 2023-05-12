837079e7bef187c2b981296ecee42bf9e033de39
royyaplc <royyap@gmail.com>
2023-05-12 17:24:44 +0800

    fixed ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lyap <lyap@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:17:34 by lyap              #+#    #+#             */
-/*   Updated: 2023/05/09 13:26:03 by lyap             ###   ########.fr       */
+/*   Updated: 2023/05/12 17:05:43 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,51 @@ descriptor.
 
 #include "libft.h"
 
+static void	ft_write_num(int n, int fd)
+{
+	int	tens;
+	int	digits;
+	int	num;
+
+	digits = 0;
+	num = n;
+	while (num != 0)
+	{
+		num /= 10;
+		++digits;
+	}
+	tens = 1;
+	while (digits != 1)
+	{
+		tens *= 10;
+		--digits;
+	}
+	while (tens != 0)
+	{
+		ft_putchar_fd((n / tens) + '0', fd);
+		n = n - n / tens * tens;
+		tens /= 10;
+	}
+	return ;
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putstr_fd(ft_itoa(n), fd);
+	if (n == 0)
+	{
+		ft_putchar_fd('0', fd);
+		return ;
+	}
+	if (n == -2147483648)
+	{
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+	}
+	ft_write_num(n, fd);
+	return ;
 }
