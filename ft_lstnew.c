73f00f99e9e038494e0d0d83bc2652140c5f62c3
royyaplc <130661038+royyaplc@users.noreply.github.com>
2023-05-13 17:00:29 +0800

    started bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyap <lyap@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 15:52:00 by lyap              #+#    #+#             */
+/*   Updated: 2023/05/13 15:52:00 by lyap             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Allocates (with malloc(3)) and returns a new node.
+The member variable ’content’ is initialized with
+the value of the parameter ’content’. The variable
+’next’ is initialized to NULL.
+*/
+
+#include "libft.h"
+
